fe7c7eb2c2084f40609e467f7905cde64a4a236c
sodiffi <44388952+sodiffi@users.noreply.github.com>
2023-06-01 19:05:55 +0800

    修改資料庫

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ from model import line
 from controller import(user, politician, proposal, manage, home, eletive)
 
 #  -----------------------
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
-)
+# from linebot import (
+#     LineBotApi, WebhookHandler
+# )
+# from linebot.exceptions import (
+#     InvalidSignatureError
+# )
+# from linebot.models import (
+#     MessageEvent, TextMessage, TextSendMessage
+# )
 from model import userModel
 from controller.util import ret
 
@@ -34,52 +34,52 @@ app.register_blueprint(proposal.proposalAPI)
 app.register_blueprint(manage.manageAPI)
 app.register_blueprint(home.homeAPI)
 app.register_blueprint(eletive.eletiveAPI)
-line_bot_api = LineBotApi(
-    "Oh4trCIb7cjh58YORcKoqqCHjKzg16U1HCCcVWrbBpplSt2LrUEzBNH+Yyjq5TWU9XG8b7LLZFxbAgYQW8nRufgVZeF4586KCRzFvxN82PLeWFEkKPbgGQwC7wV8BJ+3D5fCqFqE6f7/Js57OwQ1ZgdB04t89/1O/w1cDnyilFU=")
-handler = WebhookHandler("41121bf7a06d828660ce43a388cded2f")
+# line_bot_api = LineBotApi(
+#     "Oh4trCIb7cjh58YORcKoqqCHjKzg16U1HCCcVWrbBpplSt2LrUEzBNH+Yyjq5TWU9XG8b7LLZFxbAgYQW8nRufgVZeF4586KCRzFvxN82PLeWFEkKPbgGQwC7wV8BJ+3D5fCqFqE6f7/Js57OwQ1ZgdB04t89/1O/w1cDnyilFU=")
+# handler = WebhookHandler("41121bf7a06d828660ce43a388cded2f")
 #  -----------------------
 
 
-CORS(app)
+# CORS(app)
 
 
-@app.route('/lineLogin', methods=["GET", "OPTIONS"])
-def lineLogin():
-    userLineID = ""
-    args = request.args.get("code")
+# @app.route('/lineLogin', methods=["GET", "OPTIONS"])
+# def lineLogin():
+#     userLineID = ""
+#     args = request.args.get("code")
 
-    header = {'Content-Type': 'application/x-www-form-urlencoded'}
-    r = requests.post("https://api.line.me/oauth2/v2.1/token", headers=header, data={
-        "grant_type": "authorization_code", "code": str(args),
-        "redirect_uri": "https://test1022ntubimd.herokuapp.com/lineLogin",
-        "client_id": "1656404446",
-        "client_secret": "eb6bb1fe08f647ecf52b1e0978543a4d"
-    })
-    rText = r.text
+#     header = {'Content-Type': 'application/x-www-form-urlencoded'}
+#     r = requests.post("https://api.line.me/oauth2/v2.1/token", headers=header, data={
+#         "grant_type": "authorization_code", "code": str(args),
+#         "redirect_uri": "https://test1022ntubimd.herokuapp.com/lineLogin",
+#         "client_id": "1656404446",
+#         "client_secret": "eb6bb1fe08f647ecf52b1e0978543a4d"
+#     })
+#     rText = r.text
 
-    if("error" not in rText):
-        j = json.loads(r.text)
-        token = j["access_token"]
-        secondR = requests.get(
-            "https://api.line.me/v2/profile", headers={"Authorization": f"Bearer {token}"})
-        secondText = secondR.text
-        if("error" not in secondText):
-            secondJ = json.loads(secondText)
-            userLineID = secondJ["userId"]
-            print(userLineID)
-            data = userModel.getUserByLine(userLineID)
-            urlCond = f"?lineId={userLineID}"
-            for i in data["data"]:
-                urlCond += f'&user_id={i["id"].decode()}&identity={i["identity"]}'
+#     if("error" not in rText):
+#         j = json.loads(r.text)
+#         token = j["access_token"]
+#         secondR = requests.get(
+#             "https://api.line.me/v2/profile", headers={"Authorization": f"Bearer {token}"})
+#         secondText = secondR.text
+#         if("error" not in secondText):
+#             secondJ = json.loads(secondText)
+#             userLineID = secondJ["userId"]
+#             print(userLineID)
+#             data = userModel.getUserByLine(userLineID)
+#             urlCond = f"?lineId={userLineID}"
+#             for i in data["data"]:
+#                 urlCond += f'&user_id={i["id"].decode()}&identity={i["identity"]}'
 
-            return redirect(f"https://taipei.app/#/redirect{urlCond}")
+#             return redirect(f"https://taipei.app/#/redirect{urlCond}")
 
-        else:
-            print(secondText)
-            return ret({"success": False})
-    else:
-        print(rText)
-        return ret({"success": False})
+#         else:
+#             print(secondText)
+#             return ret({"success": False})
+#     else:
+#         print(rText)
+#         return ret({"success": False})
 
 
 @app.route("/line")
@@ -107,7 +107,7 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    data = line.lineModule.handle_messenge(event)
-    line_bot_api.reply_message(event.reply_token, data)
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     data = line.lineModule.handle_messenge(event)
+#     line_bot_api.reply_message(event.reply_token, data)
